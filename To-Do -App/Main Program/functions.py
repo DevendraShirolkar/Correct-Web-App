2c73f42282bf94f55ebb72fcdeec432312b09d44
Devendra Shirolkar <dshirolkar@gmail.com>
2023-07-30 14:19:50 +0530

    FILEPATH is added to functions.py

--- To-Do -App/Main Program/functions.py
+++ To-Do -App/Main Program/functions.py
@@ -1,4 +1,6 @@
-def get_todos(filepath='todo.txt'):
+FILEPATH = 'todo.txt'
+
+def get_todos(filepath=FILEPATH):
     """ Read the text file and return
     the list of to-do items"""
     with open(filepath, 'r') as file_local:
@@ -6,7 +8,7 @@ def get_todos(filepath='todo.txt'):
     return todos_local
 
 
-def write_todos(todos_arg, filepath='todo.txt'):
+def write_todos(todos_arg,filepath=FILEPATH):
     """Write the to-do items list in the text file"""
     with open('todo.txt', 'w') as file_local:
         todos_local = file_local.writelines(todos_arg)
