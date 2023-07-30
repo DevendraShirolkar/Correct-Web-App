c5f7aaf8af61ac7f23705d2f888a5820da3778a0
Devendra Shirolkar <dshirolkar@gmail.com>
2023-07-30 14:04:28 +0530

    Add Time feature

--- To-Do -App/Main Program/main.py
+++ To-Do -App/Main Program/main.py
@@ -1,6 +1,11 @@
 # from functions import get_todos, write_todos
 # this is another way to call function file as module
 import functions
+import time
+
+now = time.strftime("%b %d, %Y, %H %M %S")
+print("Time is as below")
+print("It is ",now)
 
 while True:
     user_action = input('Type add, show, edit, complete or exit')
